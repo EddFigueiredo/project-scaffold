cf4441de8f735da3a5a82fa427fde0c50b289148
EddFigueiredo <eddsilva@gmail.com>
2017-06-27 00:22:49 -0300

    improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 var path = require('path');
-var extractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
-var extractPlugin = new extractTextPlugin({
+var ExtractPlugin = new ExtractTextPlugin({
 	filename: 'main.css'
 });
 
@@ -9,8 +12,7 @@ module.exports = {
 	entry: './src/js/app.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/dist'
+		filename: 'bundle.js'
 	},
 	module: {
 		rules: [
@@ -27,13 +29,37 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				use: extractTextPlugin.extract({
+				use: ExtractTextPlugin.extract({
 					use: ['css-loader', 'sass-loader']
 				})
+			},
+			{
+				test: /\.html$/,
+				use: ['html-loader']
+			},
+			{
+				test: /\.(jpg|png)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'img/',
+							publicPath: 'img/'
+						}
+					}
+				]
 			}
 		]
 	},
 	plugins: [
-		extractPlugin
+		ExtractPlugin,
+		new webpack.ProvidePlugin({
+
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		}),
+		new CleanWebpackPlugin(['dist'])
 	]
 };
