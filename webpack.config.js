9487944659a180fbb60120061037b67cc318b46e
EddFigueiredo <eddsilva@gmail.com>
2017-06-27 21:32:41 -0300

    build fonts folder

--- webpack.config.js
+++ webpack.config.js
@@ -74,11 +74,11 @@ module.exports = {
 			},
 			{
 				test: /\.(woff2?|svg)$/,
-				loader: 'url-loader?limit=10000'
+				loader: 'url-loader?limit=10000&name=fonts/[name].[ext]'
 			},
 			{
 				test: /\.(ttf|eot)$/,
-				loader: 'file-loader'
+				loader: 'file-loader?name=fonts/[name].[ext]'
 			}
 		]
 	},
